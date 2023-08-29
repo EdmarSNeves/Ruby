de31e3f8b3ac8fe11962afe9e7a0d7d6a3a4841b
EDMAR S NEVES <98672053+EdmarSNeves@users.noreply.github.com>
2023-08-29 09:27:34 -0300

    Delete Projeto-final directory

--- Projeto-final/read.rb
+++
@@ -1,7 +0,0 @@
-puts "--------lista de compras----------"
-
-file = File.open('lista.txt')
-
-file.each do |line|
-    puts line
-end
--- Projeto-final/write.rb
+++
@@ -1,7 +0,0 @@
-File.open('lista.txt', 'a') do |line|
-    line.puts('Arroz')
-    line.puts('Feijao')
-    line.print('Azeite')
-    line.print(' de ')
-    line.print('Oliva')
-end
